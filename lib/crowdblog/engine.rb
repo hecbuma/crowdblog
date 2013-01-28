f5e0719178d00ef7a99207791322efc6b00bec6a
Hector Bustillos <hector.bustillos@crowdint.com>
2013-01-28 13:50:41 -0600

    load decorators

--- lib/crowdblog/engine.rb
+++ lib/crowdblog/engine.rb
@@ -2,6 +2,10 @@ module Crowdblog
   class Engine < ::Rails::Engine
     isolate_namespace Crowdblog
 
+    Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
+      Rails.configuration.cache_classes ? require(c) : load(c)
+    end
+
     initializer :assets do |config|
       Rails.application.config.assets.precompile += %w( crowdblog.css crowdblog.js )
     end
